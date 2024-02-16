34800c48190cdafaf4ae9580e3fa9288b6bbd864
HammondCameron55 <110644424+HammondCameron55@users.noreply.github.com>
2024-02-16 10:37:25 -0700

    Still working

---
+++ Mission06_Hammond/Models/MovieDatabaseContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mission06_Hammond.Models
+{
+    public class MovieDatabaseContext :DbContext
+    {
+        public MovieDatabaseContext(DbContextOptions<MovieDatabaseContext> options) : base(options) //Constructor
+        { }
+
+        public DbSet<MovieSubmit> movieSubmits { get; set; }
+    }
+}
